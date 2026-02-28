32f25ce84b0f3c444eff39755a8d1c9a1bdb324b
vucker <103271312+vucker@users.noreply.github.com>
2026-02-28 17:19:44 +0300

    Spwan

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex = 0;
+    public float spawnPosX = 20;
+    public float spawnPosZ = 5;
     void Update()
     {
-        Instantiate(animalPrefabs[animalIndex], transform.position, animalPrefabs[animalIndex].transform.rotation);
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnPosX, spawnPosX), 0f, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
