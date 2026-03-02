e672acb6b6cf240db1d99893e40ec86ce8d006f2
vucker <103271312+vucker@users.noreply.github.com>
2026-03-02 10:06:01 +0300

    Реализовано аередвижение по вертикали и горизонтали

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
-    public float xRange = 30f;
+    public Vector2 areaX = new Vector2(-23f, 23f);
+    public Vector2 areaY = new Vector2(-3f, 23f);
     public GameObject projectileObject;
-    public float horizontalInput;
+    public float horizontalInput;   
+    public float verticalInput;
     void Update()
     {
         //Передвижение по горизонтальной осм
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        verticalInput = Input.GetAxis("Vertical");
+
+
         //Игровоя зона
-        if (transform.position.x > xRange)
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        else if (transform.position.x < -xRange)
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * speed * Time.deltaTime);
+        
+
+
+        ToThrow();
+        
+    }
+    void ToThrow()
+    {
         //Заспавнить снаряд
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectileObject, transform.position, projectileObject.transform.rotation);
         }
+    }
+    void Movement()
+    {
+
     }
 }
