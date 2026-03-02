5edbd49fa6981e8f337f2538697f4041b25df80e
vucker <103271312+vucker@users.noreply.github.com>
2026-03-02 12:38:17 +0300

    Update DestroyOutOfBounds.cs

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -25,11 +25,23 @@ public class DestroyOutOfBounds : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+
         //условие для уничтожения животных и поражения
-        else if (transform.position.z < gameManager.areaZ.x)
+        else if (transform.position.x < SetGameArea().x ||
+            transform.position.x > SetGameArea().x ||
+            transform.position.z < SetGameArea().y ||
+            transform.position.z > SetGameArea().y)
         {
             Debug.Log("Game Over");
             Destroy(gameObject);
         }
+    }
+    Vector2 SetGameArea()
+    {
+        float clampX = Mathf.Clamp(transform.position.x, gameManager.areaX.x, gameManager.areaX.y);
+        float clampZ = Mathf.Clamp(transform.position.z, gameManager.areaZ.x, gameManager.areaZ.y);
+        Vector2 gameArea = new Vector3(clampX, clampZ);
+        return gameArea;
     }
 }
