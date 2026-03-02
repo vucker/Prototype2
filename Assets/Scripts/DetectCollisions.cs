f246c849404ffffbc7afaa9c94c3de8fb7e2bc5a
vucker <103271312+vucker@users.noreply.github.com>
2026-03-02 13:09:43 +0300

    Внедрена система здоровья и очков

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class DetectCollisions : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+
+        if  ( gameManager == null)
+        {
+            Debug.Log($"{nameof(gameManager)} отсуствует");
+            return;
+        }
+    }
     //Обнаружение столкновения триггера
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            if (!gameManager.IsDie())
+            {
+                gameManager.AddHP();
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Game Over");
+                Destroy(other.gameObject);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+            gameManager.AddScore();
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Настройки игровой области")]
     public Vector2 areaX = new Vector2(-23f, 23f);
     public Vector2 areaZ = new Vector2(-3f, 23f);
     public Color gizmosColor = Color.green;
-    void Update()
-    {
-        
+    [Header("Настройки счётчиков")]
+    public int maxHP = 3;
+    public int currentHP = 0;
+    public int score = 0;
 
+    private void Start()
+    {
+        currentHP = maxHP;
+        score = 0;
     }
     private void OnDrawGizmos()
     {
@@ -25,4 +31,17 @@ public class GameManager : MonoBehaviour
             areaZ.y - areaZ.x);
         Gizmos.DrawWireCube(centerVector, sizeVector);
     }
+    public void AddHP(int addAmount = -1)
+    {
+        currentHP += addAmount;
+        Debug.Log($"Текущий запас здоровья: {currentHP}");
+    }
+    public void AddScore(int addAmount = 1)
+    {
+        score += addAmount;
+        Debug.Log($"Текущий счёт: {score}");
+    }
+    public bool IsDie() => currentHP <= 0;
+
+
 }
